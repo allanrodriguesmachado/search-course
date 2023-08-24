9cfd67569c5e9d46892ef7f845e4f9eeef17eb1c
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-08-24 20:18:10 -0300

    :up:

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Admin\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
-Route::get('home', [AuthController::class, 'home'])->name('home');
